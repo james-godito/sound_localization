f24c5daf2677dc03a4a81013e278c2acc09914a0
jayyy4 <burner.acct900@gmail.com>
2024-02-08 15:05:44 +0100

    fixed beamforming without needing to rotate spectra

--- MicArraySim.py
+++ MicArraySim.py
@@ -4,6 +4,11 @@ from scipy.signal import fftconvolve
 import matplotlib.pyplot as plt
 import pyroomacoustics as pra
 
+# =============================================================================
+# TO-DO:
+# fix ill-conditioned matrix error
+# =============================================================================
+
 # Sound source locations
 azimuth = np.array([45, 135]) / 180 * np.pi # Angular displacement on a spherical coordinate system from a cardinal direction (+-180)
 distance = 5 # meters
@@ -59,10 +64,13 @@ room.add_microphone_array(mic_array)
 
 rng = np.random.RandomState(23)
 duration_samples = int(fs)
-print(azimuth)
 
+sound_loc = []
 for ang in azimuth:
-    source_location = np.array([room_x, ang, room_z/2])
+    y_loc = ang + (room_y/2 - (azimuth[1] - azimuth[0])) # adjusting sound loc to center
+    x_loc = (azimuth[1]- azimuth[0]) / len(azimuth)
+    sound_loc.append((x_loc, y_loc)) # for printing
+    source_location = np.array([x_loc, y_loc, room_z/2])
     source_signal = rng.randn(duration_samples)
     room.add_source(source_location, signal=source_signal)
     
@@ -81,7 +89,7 @@ for algo_name in algo_names:
     # the max_four parameter is necessary for FRIDA only
     doa = pra.doa.algorithms[algo_name](mic_loc, fs, nfft, c=c, num_src=2, max_four=4)
 
-    # this call here perform localization on the frames in X
+    # this call here performs localization on the frames in X
     doa.locate_sources(X, freq_range=freq_range)
     
     # store spatial response
@@ -102,40 +110,47 @@ true_col = [0, 0, 0]
 
 # loop through algos
 phi_plt = doa.grid.azimuth
-# loop through algos
 for algo_name in algo_names:
     # plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='polar')
-
-    # Adjust the azimuth angle range
-    ax.set_theta_zero_location('E')
-    # ax.set_theta_direction(-1)
-
-    # plot spatial spectrum, and rotate it by 180 degrees
-    phi_plt = doa.grid.azimuth
-    c_dirty_img = spatial_resp[algo_name]
-    ax.plot(phi_plt, base + height * np.roll(c_dirty_img, len(c_dirty_img)//2), linewidth=3,
-            alpha=0.55, linestyle='-', label="Spatial Spectrum")
-
-    # plot true locations
+    
+    c_phi_plt = np.r_[phi_plt, phi_plt[0]]
+    c_dirty_img = np.r_[spatial_resp[algo_name], spatial_resp[algo_name][0]]
+    ax.plot(c_phi_plt, base + height * c_dirty_img,
+            linewidth=3,
+            alpha=0.55,
+            linestyle='-',
+            label="spatial spectrum"
+            )
+    plt.title(algo_name)
+    
+    # plot true loc
     for angle in azimuth:
         ax.plot([angle, angle], [base, base + height], linewidth=3, linestyle='--',
-                color=true_col, alpha=0.6)
-
+            color=true_col, alpha=0.6)
     K = len(azimuth)
-    ax.scatter(azimuth, base + height * np.ones(K), c=np.tile(true_col, (K, 1)), s=500,
-               alpha=0.75, marker='*', linewidths=0, label='True Locations')
+    ax.scatter(azimuth, base + height*np.ones(K), c=np.tile(true_col,
+               (K, 1)), s=500, alpha=0.75, marker='*',
+               linewidths=0,
+               label='true locations')
 
-    # set plot properties
-    ax.set_title(algo_name)
-    ax.legend(framealpha=0.5, scatterpoints=1, loc='center right', fontsize=16,
-              ncol=1, bbox_to_anchor=(1.6, 0.5), handletextpad=.2, columnspacing=1.7, labelspacing=0.1)
-    ax.set_xticks(np.radians(np.linspace(0, 360, num=12, endpoint=False)))
+    plt.legend()
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels, framealpha=0.5,
+              scatterpoints=1, loc='center right', fontsize=16,
+              ncol=1, bbox_to_anchor=(1.6, 0.5),
+              handletextpad=.2, columnspacing=1.7, labelspacing=0.1)
+
+    ax.set_xticks(np.linspace(0, 2 * np.pi, num=12, endpoint=False))
     ax.xaxis.set_label_coords(0.5, -0.11)
     ax.set_yticks(np.linspace(0, 1, 2))
     ax.xaxis.grid(color=[0.3, 0.3, 0.3], linestyle=':')
     ax.yaxis.grid(color=[0.3, 0.3, 0.3], linestyle='--')
     ax.set_ylim([0, 1.05 * (base + height)])
-
+    
+fig, ax = room.plot()
+print("azimuth", azimuth[1] - azimuth[0])
+print("mic y, z =", y_coordinates, z_coordinates)
+print("sound location =", sound_loc)
 plt.show()
