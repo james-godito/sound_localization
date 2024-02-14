af0cd695707a17c4984d89238fa27d8e89eab2cf
VemundC <149275667+VemundC@users.noreply.github.com>
2024-02-14 14:49:44 +0100

    Add files via upload

--- Facialdetection_YOLOv5.py
+++ Facialdetection_YOLOv5.py
@@ -1,12 +1,24 @@
 import cv2
 import torch
+import serial
+import time
 
 # Load YOLOv5 model (change the path to your trained model)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
+# Initialize serial communication with Arduino
+  # Change to your Arduino's COM port
+ser = serial.Serial(port='COM3',baudrate= 9600)
+
+def write_read(x):
+    ser.write(int(x))
+    time.sleep(0.5)
+    data = ser.readline()
+    return   data
+
 def main():
     # Initialize webcam (usually index 0 for the default camera)
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Error: Webcam not found or cannot be opened.")
@@ -29,7 +41,11 @@ def main():
                 x, y, w, h = det[:4]
                 center_x = int(x + w / 2)
                 center_y = int(y + h / 2)
+                
                 print(f"Person at (x, y): ({center_x}, {center_y})")
+               
+                # Send central_x and central_y to Arduino
+                ser.write(f"{center_x},{center_y}\n".encode())
 
         # Display the frame
         cv2.imshow("Webcam Feed", frame)
@@ -41,6 +57,7 @@ def main():
     # Release the webcam and close the window
     cap.release()
     cv2.destroyAllWindows()
+    ser.close()  # Close serial connection
 
 if __name__ == "__main__":
     main()
