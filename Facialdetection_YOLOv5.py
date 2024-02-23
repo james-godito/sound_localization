e27bf85ac19d731a75e58173b3573ad509879e98
VemundC <149275667+VemundC@users.noreply.github.com>
2024-02-23 15:31:21 +0100

    Update Facialdetection_YOLOv5.py
    tidligere ødelag ser.timeout for koden, her funker koden, men grunnet for mye data som blir sendt på en gang(tror jeg) så kræsjer koden

--- Facialdetection_YOLOv5.py
+++ Facialdetection_YOLOv5.py
@@ -15,16 +15,15 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 # Initialize serial communication with Arduino (change to your Arduino's COM port)
 ser = serial.Serial(port='COM3', baudrate=9600)
-ser.timeout = 0.1
 
-def write_read(x):
-    ser.write(int(x))
-    time.sleep(0.5)
-    data = ser.readline()
-    return data
+
+
 
 def undistort_frame(frame):
-    return cv2.undistort(frame, cameraMatrix, distCoeffs)
+    undistorted_frame = cv2.undistort(frame, cameraMatrix, distCoeffs)
+    return undistorted_frame
+
+
 
 def main():
     # Initialize webcam (usually index 0 for the default camera)
@@ -56,6 +55,7 @@ def main():
                 x, y, w, h = det[:4]
                 center_x = int(x + w / 2)
                 center_y = int(y + h / 2)
+            
 
                 # Send central_x and central_y to Arduino
                 ser.write(f"{center_x},{center_y}\n".encode())
