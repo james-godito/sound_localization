a8304b95719f612f716bad616294e25d66c04971
VemundC <149275667+VemundC@users.noreply.github.com>
2024-03-01 12:24:18 +0100

    Update Facial_Servo.ino
    nærmere i guess

--- Facial_Servo.ino
+++ Facial_Servo.ino
@@ -13,7 +13,7 @@ Servo myServo;
 void setup() {
     myServo.attach(3);
     Serial.begin(9600); // Initialize serial communication
-    myServo.writeMicroseconds(1500); // Set the initial position to stop the servo
+    myServo.write(90); // Set the initial position to stop the servo
 }
 
 void loop() {
@@ -27,24 +27,15 @@ void loop() {
         if (commaPos != -1) {
             centerX = centerPxStr.substring(0, commaPos).toInt(); // Extract centerX
         }
-        
-        if(centerX<x_min || centerX>x_max){
-            if (centerX < x_min) {
-                deg+=5;
-                myServo.write(deg); // Set the servo to rotate in one direction
-            } 
-            else if (centerX > x_max) {
-                deg-=5;
-                myServo.write(deg); // Set the servo to rotate in the other direction
-            }
-            else{
-              deg=deg;
-            }
-
-            delay(10);
-        }
-        else{
-          myServo.write(deg);
-        }
     }
+
+    if(centerX<x_min && centerX != 0 && deg < 180){
+        deg+=1;
+        myServo.write(deg);
+    }
+    else if(centerX>x_max && deg > 0){
+        deg-=1;
+        myServo.write(deg);
+    }
+    delay(5);
 }
