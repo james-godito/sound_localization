bd0c2bdcc51ad1a89de6ef7dca1ca990952d46ad
james-godito <152870804+james-godito@users.noreply.github.com>
2024-05-21 20:31:43 +0200

    Update tdoa.py

--- tdoa.py
+++ tdoa.py
@@ -1,17 +1,38 @@
 """
 Generalized Cross-correlation - Phase Transform is a Time-difference-of-arrival (TDOA) Algorithm where PHAT is a well-known weighting function
 """
-def gcc_phat(sig, refsig, fs=1, interp=1000):
+from time import time
+
+# function for measuring run time of functions
+def timer_func(func): 
+    def wrap_func(*args, **kwargs):
+        times = 0
+        counter = 100
+        for i in range(counter):
+            print("running")
+            # counter += 1
+            # print(counter)
+            t1 = time() 
+            result = func(*args, **kwargs) 
+            t2 = time()
+            times += (t2 - t1)
+        print(f'Function {func.__name__!r} executed {counter} times, and took on average {times/counter:.4f}s') 
+        return result 
+    return wrap_func 
+
+# @timer_func
+def gcc_phat(sig, refsig, fs=1, max_tau=None, interp=10):
     import numpy as np
     '''
     calculates time delay given a reference signal, refsig, and another signal, sig.
     '''
     n = np.shape(sig)[0] + np.shape(refsig)[0]
-    n = 2 ** int(np.log(n)/np.log(2)) # fft is most efficient when n is a power of 2
+    
+    window = np.hanning(len(sig)) # applies hanning window before DRFFT (discrete real fast fourier transform)
     
     # applies FFT on both signals and gets the cross power spectral density (CPSD)
-    SIG = np.fft.fft(sig, n=n)
-    REFSIG = np.fft.fft(refsig, n=n)
+    SIG = np.fft.rfft(sig*window, n=n) # rfft has better performance
+    REFSIG = np.fft.rfft(refsig*window, n=n)
     G = SIG * np.conj(REFSIG)
     
     # print("G shape", np.shape(G))
@@ -22,44 +43,70 @@ def gcc_phat(sig, refsig, fs=1, interp=1000):
     
     # print("gpsi|", np.shape(G * psi))
     
-    cc = np.fft.ifft(G * psi, n=(interp * n))
+    cc = np.fft.irfft(G * psi, n=(interp * n))
     
     # print("cc", np.shape(cc))
     
     shift = int(interp * n / 2) # where to shift xcorr graph to get negative tau
-    
-    cc = np.concatenate((cc[-shift:], cc[:shift + 1]))
+    # print("cc_len", len(cc))
+    # print("shift", shift)
+    if max_tau:
+        shift = np.minimum(int(interp * fs * max_tau), shift)
 
+    cc = np.concatenate((cc[-shift:], cc[:shift + 1]))
+    # print(np.argmax(np.abs(cc)))
     # find max cross correlation index
-    argmax = np.argmax(np.abs(cc)) - shift
+    argmax = np.argmax(cc) - shift
 
     tau = argmax / float(interp * fs)
     
-    return tau, cc
+    return int(tau), cc
+
+# @timer_func
+def find_tau(cc, fs=1, max_tau=None, interp=10):
+    shift = int(len(cc) / 2)
+    argmax = np.argmax(cc) - shift
+    # print("argmax", argmax)
+    if abs(argmax) == 1:
+        argmax = 0
+        
+    tau = argmax / float(interp * fs)
+    
+    return tau
+
+from scipy import signal
+import numpy as np
+# @timer_func
+def gcc_phat_scipy(sig, refsig):
+    correlation = signal.correlate(sig, refsig)
+    lags = signal.correlation_lags(len(sig), len(refsig))
+    lag = lags[np.argmax(correlation)]
+    
+    return lag, correlation
 
 #testing (run this as main)
 def main():
     import numpy as np
     import matplotlib.pyplot as plt
+    import tdoa
+    from scipy import signal
     
-    refsig = np.linspace(1, 10, 10)
+    rng = np.random.default_rng()
+    sig = rng.standard_normal(999)
     
-    for i in range(0, 10):
-        sig = np.concatenate((np.linspace(0, 0, i), refsig, np.linspace(0, 0, 10 - i)))
-        ref = np.concatenate((np.linspace(0, 0, 10 - i), refsig, np.linspace(0, 0, i)))
-        # ref = np.array([0, 0, 0, 0, 1, 2, 3, 4])
-        print("signal: ", sig)
-        print("\n")
-        print("ref. signal: ", ref)
-        offset, _ = gcc_phat(sig, ref)
-        print("offset", offset)
-        print("-----------------------------------")
+    sig1 = np.concatenate((rng.standard_normal(99), sig[:900]))
+    sig2 = sig
+    sig3 = np.concatenate((rng.standard_normal(99), sig[:900]))
+    sig4 = np.concatenate((rng.standard_normal(399), sig[:600]))
+    sig5 = np.concatenate((rng.standard_normal(199), sig[:800]))
     
-    print("\n")
-    print(np.shape(ref))
-    print(np.shape(sig))
-    print(np.shape(_))
-    plt.plot(_)
+    
+    lag12 = tdoa.gcc_phat_scipy(sig2, sig4)
+    
+    lag12, _ = tdoa.gcc_phat(sig2, sig1)
+    print(lag12)
+    print(len(_))
+    
         
 if __name__ == "__main__":
     main()
